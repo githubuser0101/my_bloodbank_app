b200676b0c3ce80b4aac98954246a653a5596f13
https://github.com/githubuser0101/flutterorigins.git <nowmailsonuchauhan@gmail.com>
2023-02-01 22:41:31 +0530

    first change

---
+++ android/app/src/main/kotlin/com/example/my_bloodbank_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_bloodbank_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// ignore: unused_import
+import 'package:flutter/material.dart';
+
+import 'flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SafeArea(child: Text("Hello BloodBankApp"),)
+    )));
+}
